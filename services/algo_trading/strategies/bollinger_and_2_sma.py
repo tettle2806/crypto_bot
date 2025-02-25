ec8e6ffad6d18f6fffbe2e2649f717622d8f91a4
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2025-02-25 17:25:01 +0500

    надо дработать функцию

--- services/algo_trading/strategies/bollinger_and_2_sma.py
+++ services/algo_trading/strategies/bollinger_and_2_sma.py
@@ -2,7 +2,10 @@
 Используется две кривые тренда.
 Если быстрая скольящая средняя выше медленного скользящего среднего, то тренд восходящий.
 И используем края полосы боллинджера для определения точек входа в позу."""
-
+#TODO   Функция не полностью работает в будущем её нужно доработать.
+#TODO   Проблема в том что она не открывает сделки, но когда данные берутся из файла, то всё работает.
+#TODO
+#TODO
 
 import numpy as np
 import pandas as pd
@@ -151,40 +154,39 @@ fig.add_scatter(x=dfpl.index, y=dfpl['pointpos'], mode="markers",
 fig.show()
 
 def SIGNAL():
-    return df.TotalSignal
+    return df["TotalSignal"].to_numpy()
+
 
 
 class MyStrat(Strategy):
     mysize = 3000
     slcoef = 1.1
     TPSLRatio = 1.5
-    rsi_length = 16
 
     def init(self):
         super().init()
         self.signal1 = self.I(SIGNAL)
-        # df['RSI']=ta.rsi(df.Close, length=self.rsi_length)
 
     def next(self):
         super().next()
+
+        # Проверяем, есть ли ATR
+        if np.isnan(self.data.ATR[-1]):
+            return
+
         slatr = self.slcoef * self.data.ATR[-1]
         TPSLRatio = self.TPSLRatio
 
-        # if len(self.trades)>0:
-        #     if self.trades[-1].is_long and self.data.RSI[-1]>=90:
-        #         self.trades[-1].close()
-        #     elif self.trades[-1].is_short and self.data.RSI[-1]<=10:
-        #         self.trades[-1].close()
-
-        if self.signal1 == 2 and len(self.trades) == 0:
+        if self.signal1[0] == 2 and len(self.trades) == 0:
             sl1 = self.data.Close[-1] - slatr
             tp1 = self.data.Close[-1] + slatr * TPSLRatio
             self.buy(sl=sl1, tp=tp1, size=self.mysize)
 
-        elif self.signal1 == 1 and len(self.trades) == 0:
+        elif self.signal1[0] == 1 and len(self.trades) == 0:
             sl1 = self.data.Close[-1] + slatr
             tp1 = self.data.Close[-1] - slatr * TPSLRatio
             self.sell(sl=sl1, tp=tp1, size=self.mysize)
+
 
 
 bt = Backtest(df, MyStrat, cash=250, margin=1 / 30)
